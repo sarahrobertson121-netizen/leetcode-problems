3c4368cccd79245e13503eb6ed4ea84792af64ec
Sarah Robertson <sarah.robertson121@gmail.com>
2025-11-11 21:01:50 +0000

    detail JS Create_Iterators_forEach File

---
+++ JavaScript Codecademy Course/Iterators_forEach.js
@@ -0,0 +1,10 @@
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+fruits.forEach(function (fruitsItem) {
+  console.log(`I want to eat a ${fruitsItem}`);
+});
+/*Iterate over the fruits array to log I want to eat a plus the name of 
+each fruit to the console. For example, I want to eat a mango.
+
+You may use any form of callback you prefer.*/
